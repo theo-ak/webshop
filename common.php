af857ba18c790412f323744d0ec7027cf57e7487
Denisa Badarau <denisa.badarau@5psolutions.eu>
2022-07-11 17:51:31 +0300

    #4 index.php http://pm.5psolutions.net/public/index.php?path_info=projects/training-teodora-acatrinei/tasks/4 showing only items that are not in cart

--- common.php
+++ common.php
@@ -6,9 +6,21 @@ try {
     $connection = new PDO("mysql:host=" . SERVER . ";dbname=" .  DBNAME,
         USERNAME,
         PASSWORD);
-
-    echo "Connected succesfully.";
 } catch(PDOException $err) {
     echo 'Connection failed: ' . $err->getMessage();
 }
+
+function dd($var) {
+    echo "<pre>";
+    var_dump($var);
+    echo "</pre>";
+}
+
+function selectAll($connection, $table) {
+    $sql = "SELECT * FROM $table";
+    $query = $connection->prepare($sql);
+    $query->execute();
+    return $query->fetchAll();
+}
+
 
--- index.php
+++ index.php
@@ -1,4 +1,10 @@
-<?php require_once 'common.php'; ?>
+<?php
+
+require_once 'common.php';
+
+$items = selectAll($connection, 'products');
+
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -6,9 +12,43 @@
 <head>
     <meta charset="utf-8">
     <title>Music Shop</title>
+    <!-- JavaScript Bundle with Popper -->
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 </head>
 
 <body>
+
+    <table class="table">
+        <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Title</th>
+            <th scope="col">Description</th>
+            <th scope="col">Price</th>
+        </tr>
+        </thead>
+        <tbody>
+
+        <?php foreach ($items as $item): ?>
+            <?php if(!in_array($item['id'], $_SESSION['cart'])): ?>
+                <tr>
+                    <th scope="row"><?= $item['id']; ?></th>
+                    <td><?= $item['title']; ?></td>
+                    <td><?= $item['description']; ?></td>
+                    <td><?= $item['price']; ?></td>
+                    <td>
+                        <form action="">
+                            <input type="hidden" name="id" value="<?= $item['id']; ?>">
+                            <input type="submit" value="Add" class="btn btn-primary">
+                        </form>
+                    </td>
+                </tr>
+            <?php endif; ?>
+
+        <?php endforeach; ?>
+        </tbody>
+    </table>
 
 </body>
 

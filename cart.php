7ffacdcf086fdef52d1222e4a06b92bf572d6886
Denisa Badarau <denisa.badarau@5psolutions.eu>
2022-07-12 12:49:33 +0300

    #5 cart.php http://pm.5psolutions.net/public/index.php?path_info=projects/training-teodora-acatrinei/tasks/5 added the cart form

--- cart.php
+++ cart.php
@@ -4,9 +4,22 @@ require_once 'common.php';
 
 $items = selectAll($connection, 'products');
 
+$name = $contact = $comments = "";
+$nameErr = $contactErr = $commentsErr = "";
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $index = array_search($_POST['id'], $_SESSION['cart']);
-    array_splice($_SESSION['cart'], $index, 1);
+    if (isset($_POST['id'])) {
+        $index = array_search($_POST['id'], $_SESSION['cart']);
+        array_splice($_SESSION['cart'], $index, 1);
+    }
+
+    $name = isset($_POST['name']) ? test_input($_POST['name']) : "";
+    $contact = isset($_POST['contact']) ? test_input($_POST['contact']) : "";
+    $comments = isset($_POST['comments']) ? test_input($_POST['comments']) : "";
+
+    $nameErr = $name ? "" : "Name is required";
+    $contactErr = $contact ? "" : "Contact is required";
+    $commentsErr = $comments ? "" : "Comments are required";
 }
 
 ?>
@@ -24,38 +37,58 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 <body>
 
-<table class="table">
-    <thead>
-    <tr>
-        <th scope="col">#</th>
-        <th scope="col">Title</th>
-        <th scope="col">Description</th>
-        <th scope="col">Price</th>
-    </tr>
-    </thead>
-    <tbody>
+    <table class="table">
+        <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Title</th>
+            <th scope="col">Description</th>
+            <th scope="col">Price</th>
+        </tr>
+        </thead>
+        <tbody>
 
-    <?php foreach ($items as $item): ?>
-        <?php if(in_array($item['id'], $_SESSION['cart'])): ?>
-            <tr>
-                <th scope="row"><?= $item['id']; ?></th>
-                <td><?= $item['title']; ?></td>
-                <td><?= $item['description']; ?></td>
-                <td><?= $item['price']; ?></td>
-                <td><div class="img"><img src="<?= $item['img']; ?>"></div></td>
-                <td>
-                    <form method="post" action="cart.php">
-                        <input type="hidden" name="id" value="<?= $item['id']; ?>">
-                        <input type="submit" value="Remove" class="btn btn-primary">
-                    </form>
-                </td>
-            </tr>
-        <?php endif; ?>
-    <?php endforeach; ?>
-    </tbody>
-</table>
+        <?php foreach ($items as $item): ?>
+            <?php if(in_array($item['id'], $_SESSION['cart'])): ?>
+                <tr>
+                    <th scope="row"><?= $item['id']; ?></th>
+                    <td><?= $item['title']; ?></td>
+                    <td><?= $item['description']; ?></td>
+                    <td><?= $item['price']; ?></td>
+                    <td><div class="img"><img src="<?= $item['img']; ?>"></div></td>
+                    <td>
+                        <form method="post" action="cart.php">
+                            <input type="hidden" name="id" value="<?= $item['id']; ?>">
+                            <input type="submit" value="Remove" class="btn btn-primary">
+                        </form>
+                    </td>
+                </tr>
+            <?php endif; ?>
+        <?php endforeach; ?>
+        </tbody>
 
-<a href="index.php "> <button type="button" class="btn btn-primary">Go to index</button></a>
+    </table>
+
+    <form method="post" id="details-form" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+        <div class="form-group">
+            <label for="name">Name</label>
+            <input type="text" class="form-control" id="name" name="name" placeholder="Enter name" value="<?= $name ?>">
+            <span class="error">* <?= $nameErr;?></span>
+        </div>
+        <div class="form-group">
+            <label for="contact">Contact details</label>
+            <input type="text" class="form-control" id="contact" name="contact" placeholder="Enter contact details" value="<?= $contact ?>">
+            <span class="error">* <?= $contactErr;?></span>
+        </div>
+        <div class="form-group">
+            <label for="comments">Comments</label>
+            <input type="text" class="form-control" id="comments" name="comments" placeholder="Enter comments" value="<?= $comments ?>">
+            <span class="error">* <?= $commentsErr;?></span>
+        </div>
+        <button type="submit" class="btn btn-primary">Checkout</button>
+    </form>
+
+    <a href="index.php "> <button type="button" class="btn btn-primary">Go to index</button></a>
 
 </body>
 
--- common.php
+++ common.php
@@ -17,4 +17,11 @@ function selectAll($connection, $table) {
     return $query->fetchAll();
 }
 
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    $data = strip_tags($data);
+    return $data;
+}
 

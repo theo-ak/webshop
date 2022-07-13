8cfaa087bade9adb587274d738c797790920c31e
Denisa Badarau <teodora.acatrinei@5psolutions.eu>
2022-07-13 11:39:15 +0300

    #8 product.php http://pm.5psolutions.net/public/index.php?path_info=projects/training-teodora-acatrinei/tasks/8 added the add product functionality to product.php

--- product.php
+++ product.php
@@ -11,12 +11,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     if (isset($_FILES['fileToUpload'])) {
         $target_dir = "img/";
-        $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+        $img = $target_dir . basename($_FILES["fileToUpload"]["name"]);
         $uploadOk = 1;
-        $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+        $imageFileType = strtolower(pathinfo($img, PATHINFO_EXTENSION));
 
         $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-        if($check !== false) {
+        if ($check !== false) {
             echo "File is an image - " . $check["mime"] . ".";
             $uploadOk = 1;
         } else {
@@ -26,11 +26,24 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
         if ($uploadOk == 0) {
             echo "Sorry, your file was not uploaded.";
-        } else if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-            echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
         } else {
-            echo "Sorry, there was an error uploading your file.";
+            if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $img)) {
+                echo "The file " . htmlspecialchars(basename($_FILES["fileToUpload"]["name"])) . " has been uploaded.";
+            } else {
+                echo "Sorry, there was an error uploading your file.";
+            }
         }
+    }
+
+    if ($title && $description && $price && $img) {
+        $sql = 'INSERT INTO products (title, description, price, img) VALUES (:title, :description, :price, :img)';
+        $query = $connection->prepare($sql);
+        $query->execute([
+            'title' => $title,
+            'description' => $description,
+            'price' => $price,
+            'img' => $img
+        ]);
     }
 }
 
@@ -43,31 +56,42 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="utf-8">
     <title>Music Shop</title>
     <!-- CSS only -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
 
-    <form method="post" id="details-form" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-        <div class="form-group">
-            <label for="title">Title</label>
-            <input type="text" class="form-control" id="title" name="title" placeholder="Enter title" value="<?= $title; ?>" required>
-        </div>
-        <div class="form-group">
-            <label for="description">Description</label>
-            <input type="text" class="form-control" id="description" name="description" placeholder="Enter description" value="<?= $description; ?>" required>
-        </div>
-        <div class="form-group">
-            <label for="price">Price</label>
-            <input type="number" step=".01" class="form-control" id="price" name="price" placeholder="Enter price" value="<?= $price ?>" required>
-        </div>
+<form method="post" id="details-form" action="<?php
+echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" enctype="multipart/form-data">
+    <div class="form-group">
+        <label for="title">Title</label>
+        <input type="text" class="form-control" id="title" name="title" placeholder="Enter title" value="<?= $title; ?>"
+               required>
+    </div>
+    <div class="form-group">
+        <label for="description">Description</label>
+        <input type="text" class="form-control" id="description" name="description" placeholder="Enter description"
+               value="<?= $description; ?>" required>
+    </div>
+    <div class="form-group">
+        <label for="price">Price</label>
+        <input type="number" step=".01" class="form-control" id="price" name="price" placeholder="Enter price"
+               value="<?= $price ?>" required>
+    </div>
 
+    <div class="form-group">
         Select image to upload:
         <input type="file" name="fileToUpload" id="fileToUpload" required>
+    </div>
 
+    <div class="form-group">
         <button type="submit" class="btn btn-primary" id="save">Save</button>
-    </form>
+        <a href="products.php"><button class="btn btn-primary" type="button">Back to products page</button></a>
+    </div>
+
+</form>
 
 </body>
 

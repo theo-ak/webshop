c969f3e27d2c950f1f353c2ddc72626bf70eb395
Denisa Badarau <teodora.acatrinei@5psolutions.eu>
2022-07-20 16:20:13 +0300

    #4 index.php http://pm.5psolutions.net/public/index.php?path_info=projects/training-teodora-acatrinei/tasks/4 moved the translations from common.php to translations.php

--- common.php
+++ common.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once 'config.php';
+require_once 'translations.php';
 
 session_start();
 
@@ -19,33 +20,6 @@ try {
 }
 
 define('LANGUAGE', substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2));
-
-define('TRANSLATIONS', [
-    'add' => 'adauga',
-    'title' => 'titlu',
-    'description' => 'descriere',
-    'price' => 'pret',
-    'name' => 'nume',
-    'contact details' => 'date de contact',
-    'comments' => 'comentarii',
-    'items' => 'produse',
-    'total' => 'total',
-    'date' => 'data',
-    'admin page' => 'pagina admin',
-    'go to cart' => 'cart',
-    'orders page' => 'pagina comenzi',
-    'add product' => 'adauga produs',
-    'go to index' => 'index',
-    'remove' => 'sterge',
-    'index page' => 'index',
-    'edit' => 'editeaza',
-    'delete' => 'sterge',
-    'save' => 'salveaza',
-    'back to products page' => 'pagina produse',
-    'select image to upload' => 'selecteaza imaginea',
-    'current image' => 'imaginea curenta',
-    'view order' => 'vizualizeaza comanda'
-]);
 
 function translate($label)
 {
---
+++ translations.php
@@ -0,0 +1,28 @@
+<?php
+
+define('TRANSLATIONS', [
+    'add' => 'adauga',
+    'title' => 'titlu',
+    'description' => 'descriere',
+    'price' => 'pret',
+    'name' => 'nume',
+    'contact details' => 'date de contact',
+    'comments' => 'comentarii',
+    'items' => 'produse',
+    'total' => 'total',
+    'date' => 'data',
+    'admin page' => 'pagina admin',
+    'go to cart' => 'cart',
+    'orders page' => 'pagina comenzi',
+    'add product' => 'adauga produs',
+    'go to index' => 'index',
+    'remove' => 'sterge',
+    'index page' => 'index',
+    'edit' => 'editeaza',
+    'delete' => 'sterge',
+    'save' => 'salveaza',
+    'back to products page' => 'pagina produse',
+    'select image to upload' => 'selecteaza imaginea',
+    'current image' => 'imaginea curenta',
+    'view order' => 'vizualizeaza comanda'
+]);

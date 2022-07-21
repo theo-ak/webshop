d91f594636a06ccd03cfbd154124a1226b48c68d
Denisa Badarau <teodora.acatrinei@5psolutions.eu>
2022-07-21 12:10:51 +0300

    #5 cart.php http://pm.5psolutions.net/public/index.php?path_info=projects/training-teodora-acatrinei/tasks/5 changed the testInput function to execute only htmlentities and strip_tags

--- common.php
+++ common.php
@@ -55,10 +55,8 @@ function selectById(
 
 function testInput($data)
 {
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    $data = strip_tags($data);
-    return $data;
+    return htmlentities(
+        strip_tags($data)
+    );
 }
 

bc534aa3e468a28233affc30282e4a9b0fb701eb
Denisa Badarau <teodora.acatrinei@5psolutions.eu>
2022-07-26 10:45:49 +0300

    #6 login.php http://pm.5psolutions.net/public/index.php?path_info=projects/training-teodora-acatrinei/tasks/6 added validation to login page

--- login.php
+++ login.php
@@ -7,6 +7,8 @@ if ($_SESSION['admin_logged_in']) {
     exit;
 }
 
+$_SESSION['login_err'] = '';
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = isset($_POST['username']) ? testInput($_POST['username']) : '';
     $password = isset($_POST['password']) ? testInput($_POST['password']) : '';
@@ -23,6 +25,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             header('location: products.php');
             exit;
         }
+    } else {
+        $_SESSION['login_err'] = 'Invalid credentials.';
     }
 }
 
@@ -31,6 +35,7 @@ require 'header.php';
 ?>
 
     <form method="post" id="details-form" action="login.php">
+        <span><?= isset($_SESSION['login_err']) ? $_SESSION['login_err'] : ''; ?></span>
         <div class="form-group">
             <label for="username"><?= translate('Username') ?></label>
             <input type="text" class="form-control" id="username" name="username" placeholder="Username" required>
